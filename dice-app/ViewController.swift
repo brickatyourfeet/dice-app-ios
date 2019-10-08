e1564b4b10ca69ad6b92d9e48ab2c1d88db3d2ab
brickatyourfeet <brickatyourfeet@gmail.com>
2019-10-08 14:46:01 -0700

    add randomized dice on roll

--- dice-app/ViewController.swift
+++ dice-app/ViewController.swift
@@ -10,15 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDiceIndex: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateDiceImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages(){
+        randomDiceIndex = Int.random(in: 0...5)
+        randomDiceIndex2 = Int.random(in: 0...5)
+        
+        diceImageView1.image = UIImage(named: "dice\(randomDiceIndex + 1)")
+        diceImageView2.image = UIImage(named: "dice\(randomDiceIndex2 + 1)")
     }
     
 }
